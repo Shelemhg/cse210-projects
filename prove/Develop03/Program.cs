3899a5d59687e00b77259c69d16fa1889d83032a
Shelemhg <48326251+Shelemhg@users.noreply.github.com>
2023-05-13 11:39:48 -0600

    deleted local journal test file

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,21 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-                // do{
-        //     DisplayMenu();
-        // } while(number != 0);
-        string selection;
-        Console.WriteLine("1. New Entry");
-
-        do {
-            Console.WriteLine("       J O U R N A L");
-            Console.WriteLine("1. New Entry");
-            Console.WriteLine("1. New Entry");
-            Console.WriteLine("1. New Entry");
-            Console.WriteLine("1. New Entry");
-            Console.WriteLine("0. Exit");
-            selection = Console.ReadLine();
-
-        } while (selection != "0");
+        Console.WriteLine("Hello Develop03 World!");
     }
 }
