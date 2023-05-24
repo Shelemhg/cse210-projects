e8f492b2220976ea0f7d373c4dd76536545aa66a
Shelemhg <48326251+Shelemhg@users.noreply.github.com>
2023-05-23 20:19:51 -0600

    Added a couple extra warnings when empty save

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -59,61 +59,75 @@ class Program
 
                 //   Display Journal
                 case "2":
-                    
-                    if(journal1._entries.Count() > 0){
+                    if(File.Exists(Journal._fileName)){
+                        if(journal1._entries.Count() > 0){
+                            Console.Clear();
+                            Journal.DisplayJournal(journal1);
+                        }
+                        else{
+                            Console.Clear();
+                            Console.WriteLine("\nCurrent journal is empty.\n\nTry loading journal from file or adding a new entry.\n");
+                        }
+                    }else{
                         Console.Clear();
-                        Journal.DisplayJournal(journal1);
-                    }
-                    else{
-                        Console.WriteLine("\nCurrent journal is empty. Try loading journal from file or adding a new entry.\n");
+                        Console.WriteLine("No Journal file was found.\n\nTry creating a new one or moving the journal to the program folder.\n");
                     }
                 break;
 
                 //   Read from File
                 case "3":
-                    
-                    Console.Clear();
-                    if(journal1._entries.Count > 0){
-                        string res;
-                        do{
-                            Console.WriteLine("By loading a new file the follow information will be permanently deleted:");                    
-                            Journal.DisplayJournal(journal1);
-                            Console.WriteLine("\nDo you wish to continue (y/n):");
-                            res = Console.ReadLine();
-                            
-                            if(res.ToLower() == "y"){
-                                journal1._entries.Clear();
-                                Journal.ReadFromFile(journal1);
-                            }
-                            else if (res.ToLower() == "n"){
-                                break;
-                            }
-                        } while (res.ToLower() != "y" && res.ToLower() != "n");
-                    }
-                    else{    
-                        Journal.ReadFromFile(journal1);
-                    }            
-                        Console.Clear();     
-                        Journal.DisplayJournal(journal1);        
+                    if(File.Exists(Journal._fileName)){
+                        Console.Clear();
+                        if(journal1._entries.Count > 0){
+                            string res;
+                            do{
+                                Console.WriteLine("By loading a new file the follow information will be permanently deleted:");                    
+                                Journal.DisplayJournal(journal1);
+                                Console.WriteLine("\nDo you wish to continue (y/n):");
+                                res = Console.ReadLine();
+                                
+                                if(res.ToLower() == "y"){
+                                    journal1._entries.Clear();
+                                    Journal.ReadFromFile(journal1);
+                                }
+                                else if (res.ToLower() == "n"){
+                                    break;
+                                }
+                            } while (res.ToLower() != "y" && res.ToLower() != "n");
+                        }
+                        else{    
+                            Journal.ReadFromFile(journal1);
+                        }            
+                            Console.Clear();     
+                            Journal.DisplayJournal(journal1);   
+                    }else{
+                        Console.Clear();
+                        Console.WriteLine("No Journal file was found.\n\nTry creating a new one or moving the journal to the program folder.\n");
+                    }   
                 break;
 
                 //    Save to File
                 case "4":
                     
                     if(File.Exists(Journal._fileName)){
-                        string res;
-                        do {
-                            Console.WriteLine("By saving recent entries the current information in the existing file will be permanently deleted.\n\nDo you wish to continue? (y/n):");
-                            res = Console.ReadLine();
-                            if (res.ToLower() == "y"){
-                                Console.Clear();
-                                Journal.SaveToFile(journal1);
-                                Console.WriteLine("Journal saved on file.");
-                            }
-                            else{
-                                Console.Clear();
-                            }
-                        } while (res.ToLower() != "y" && res.ToLower() != "n");
+                        if(journal1._entries.Count > 0){
+                            string res;
+                            do {
+                                Console.WriteLine("By saving recent entries the current information in the existing file will be permanently deleted.\n\nDo you wish to continue? (y/n):");
+                                res = Console.ReadLine();
+                                if (res.ToLower() == "y"){
+                                    Console.Clear();
+                                    Journal.SaveToFile(journal1);
+                                    Console.WriteLine("Journal saved on file.");
+                                }
+                                else{
+                                    Console.Clear();
+                                }
+                            } while (res.ToLower() != "y" && res.ToLower() != "n");
+                        }else{
+                            Console.Clear();
+                            Console.WriteLine("No new entry was found.\n\nTry creating a new one by selecting option 1.\n");
+                        }
                     }
                     else{
                         Console.Clear();
